869d2955510fd1ce951c80ec20da5aade55f91ff
Shaun Merrill <Shaun.Merrill@PSE.com>
2020-10-27 17:29:54 -0700

    AdvanceStmtAcctHistory
    Signed-off-by: Shaun Merrill <Shaun.Merrill@PSE.com>

---
+++ app/store/AdvanceStmtAcctHistoryStore.js
@@ -0,0 +1,25 @@
+Ext.define("Test.store.AdvanceStmtAcctHistoryStore", {
+  // see http://sqewtmdlt01v02/MPARestService/Help/Api/GET-api-AdvanceStmtAcctHistory_statementAccountId 
+    extend: "Ext.data.Store",
+    alias: "store.advanceStmtAcctHistoryStore",
+    storeId: "advanceStmtAcctHistoryStore",
+    model: "Test.model.advanceStmtAcctHistory",
+  
+    autoLoad: false,
+  
+    proxy: {
+      type: "jsonp",
+      url: MySharedData.serverUrl + "AdvanceStmtAcctHistory",
+      reader: {
+        type: "json",
+      },
+      headers: {
+        Accept: "application/json",
+        Origin: "http://sqewtmdlt01v02/MDL",
+      },
+      actionMethods: {
+        read: "GET",
+      },
+    },
+  });
+  
